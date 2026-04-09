bde13814952d48f0be218bb373ccbb21d77a0e54
Asterrixxxxx <tobleroneallalone@gmail.com>
2026-04-09 19:50:42 +0100

    Added shoes and final mouse art, still working on animation

--- Assets/Code/Mouse.cs
+++ Assets/Code/Mouse.cs
@@ -8,6 +8,8 @@ public class Mouse : MonoBehaviour
     public Texture2D MouseTest1;
     private Camera cam;
     [SerializeField] AudioClip ClickSound;
+    [SerializeField] public Animator animator;
+
 
     void Start()
     {
@@ -19,11 +21,15 @@ public class Mouse : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Mouse0)){
         AudioSource.PlayClipAtPoint(ClickSound, Vector3.zero);
         Cursor.SetCursor(MouseTest1, Vector2.zero, CursorMode.ForceSoftware);
+        animator.SetBool("isplaying",true);
+        var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position; 
+        animator.bodyPosition = mousePos;
         }
 
         if (Input.GetKeyUp(KeyCode.Mouse0))
         {
             Cursor.SetCursor(MouseTest, Vector2.zero, CursorMode.ForceSoftware);
+            animator.SetBool("isplaying",false);
         }
     }
 }
